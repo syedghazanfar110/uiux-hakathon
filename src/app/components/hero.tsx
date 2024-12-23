310b539d3df3c4c250abac24c43d330906bbddb2
Syed Ghazanfar Abbas Zaidi <syedabbaszaidi516@gmail.com>
2024-12-23 23:21:23 +0500

    new file name add
    hero

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,19 @@
+
+import Image from 'next/image';
+import React from 'react';
+
+const Hero = ()  => {
+  return (
+    <div className="flex justify-center items-center h-[505px] w-[1440px] t-29px left-54px">
+      <Image
+        src={"/assets/logo.png" }// Aapke public folder mein image ka path
+        alt="Logo"
+        width={200} // Required width
+        height={200} // Required height
+        className="h-[505px] w-[1440px] t-29px left-54px"
+      />
+    </div>
+  )
+}
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Logo from "./components/navbar";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
    <div>
     <Navbar />
     <Footer />
-    <Logo />
+    <Hero />
    </div>
   );
 }
