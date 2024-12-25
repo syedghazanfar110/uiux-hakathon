957fbb7d9722ccf4c20abf557f92b849edb87bca
Syed Ghazanfar Abbas Zaidi <syedabbaszaidi516@gmail.com>
2024-12-25 21:46:31 +0500

    new change
    navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from './hero';
 
 
 
@@ -23,11 +24,14 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center h-[100px] w-full px-6">
+    <div className="flex justify-evenly space-x-8 justify- items-center px-6">
+      <div className="flex items-center space-x-8 h-[100px] w-[1440px] " >
+        <Hero />
       <div>
       
-      </div>
       <h2 className="font-montserrat text-[24px] font-bold">Furniro</h2>
+      
+      </div>
       <ul className="flex items-center space-x-8 font-poppins text-[16px]">
         {navItems.map((item, index) => (
           <li key={index} className="hover:text-[#B8B8B8]">
@@ -35,6 +39,14 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      
+      </div>
+      <div>
+        <ul className="flex items-center space-x-8 font-poppins text-[16px]">
+          <li className="hover:text-[#B8B8B8]">Login</li>
+          <li className="hover:text-[#B8B8B8]">Register</li>
+        </ul>
+    </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 
 import Footer from "./components/footer";
-import Hero from "./components/hero";
 
 export default function Home() {
   return (
    <div>
     
+    <div>
     <Footer />
-    <Hero />
+    </div>
+    
+    
+
     
    </div>
   );
